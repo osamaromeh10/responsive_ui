14ef022c03b8ccd4a506cb7bf40c97e72eec307a
Osama Romeh <osamaromeh42@gmail.com>
2025-08-02 14:11:48 +0300

    CreateCustomSliverGrid

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_ui/views/widgets/home_view_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -7,10 +8,12 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Color(0xffD8D8D8),
         appBar: AppBar(
           backgroundColor: Colors.black,
           leading: Icon(Icons.menu, color: Colors.white),
         ),
+        body: HomeViewBody(),
       ),
     );
   }
---
+++ lib/views/widgets/home_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_ui/views/widgets/custom_sliver_grid.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(child: SizedBox(height: 15)),
+        CustomSliverGrid(),
+      ],
+    );
+  }
+}
