232e4f5598b523dcdaeaa4596a29b2ebf56caf1d
Osama Romeh <osamaromeh42@gmail.com>
2025-08-03 11:15:08 +0300

    CreateCustomDrawerItem and DrawerItemModel

--- lib/views/widgets/custom_drawer.dart
+++ lib/views/widgets/custom_drawer.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:responsive_ui/views/widgets/custom_drawer_item.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: [
           DrawerHeader(child: Icon(FontAwesomeIcons.solidHeart, size: 32)),
+          CustomDrawerItem(drawerItemModel: null),
         ],
       ),
     );
